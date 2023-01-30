21d372e7d3ed4817dff5e107099c4b646cd6d808
gopi2405 <mgopireddy0@gmail.com>
2023-01-30 17:56:42 +0530

    first commit

--- routes/forget.js
+++ routes/forget.js
@@ -4,13 +4,11 @@ module.exports = (() => {
         var psw = req.body.psw;
         var cpsw = req.body.cpsw;
         global.db_con.query("SELECT * FROM users", function (err, result) {
-            arr = result.find(user => user.email == email);
-            if (arr.email == email) {
+            var arr = result.find(user => user.email == email);
+            if (typeof arr !== "undefined") {
                 if (psw == cpsw) {
                     var update = "update users set psw = ?, cpsw = ? where email = ?";
                     global.db_con.query(update, [psw, cpsw, email]);
-                    let arr1 = result.find(user => user.email == email);
-                    console.log(arr1);
                     res.render('forget', { alert: 'msg' })
                 } else {
                     res.render('forget', { alert: 'psw' });
